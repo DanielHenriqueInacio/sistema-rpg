501f2d5ccdd68719447342f18dcadd2a59fcd88d
Daniel Inácio <danielhi2013@hotmail.com>
2022-09-09 19:54:23 -0300

    corrigindo bug

--- front/src/App/Services/equipamentoService.js
+++ front/src/App/Services/equipamentoService.js
@@ -1,8 +1,8 @@
 import axios from "../../config/axios.js";
 
-const listarArmas = () => axios.get("/equipamentos?filters[tipo_equipamento][$eq]=arma");
-const listarArmaduras = () => axios.get("/equipamentos?filters[tipo_equipamento][$eq]=armadura");
-const listarOrnamentos = () => axios.get("/equipamentos?filters[tipo_equipamento][$eq]=ornamento");
+const listarArmas = () => axios.get("/equipamentos?filters[tipo_equipamento][$eq]=arma&populate=deep");
+const listarArmaduras = () => axios.get("/equipamentos?filters[tipo_equipamento][$eq]=armadura&populate=deep");
+const listarOrnamentos = () => axios.get("/equipamentos?filters[tipo_equipamento][$eq]=ornamento&populate=deep");
 
 export {
     listarArmas,
