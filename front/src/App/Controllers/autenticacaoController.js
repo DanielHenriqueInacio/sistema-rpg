7381d60318a6fdf1442c41d6a24a2de2e8079817
Daniel Inácio <danielhi2013@hotmail.com>
2022-09-14 21:48:18 -0300

    continuando validacao do login e cadastro

--- front/src/App/Controllers/autenticacaoController.js
+++ front/src/App/Controllers/autenticacaoController.js
@@ -1,12 +1,17 @@
-import {cadastrarJogador, loginAll, pegarJogadorPorEmail} from "../Services/jogadorService.js";
+import {cadastrarJogador, loginAll, pegarJogadorPorEmail, pegarJogadorPorToken, alterarJogador} from "../Services/jogadorService.js";
 import {validarCadastroJogador, validarLoginJogador} from "../Validators/Jogadores/jogadorValidator.js";
+import mailer from "../../config/mailer.js";
+import {uuid} from "uuidv4";
 
 const cadastro = async (request, response) => {
+    console.log(uuid())
     const objVazio = Object.keys(request.body).length === 0;
     if (!objVazio) {
+        const token = uuid();
         const dados = {
             data: {
-                ...request.body
+                ...request.body,
+                token
             }
         }
         try {
@@ -14,7 +19,7 @@ const cadastro = async (request, response) => {
             await validarCadastroJogador().validate(request.body, {abortEarly: false});
             const jogadorExiste = await pegarJogadorPorEmail(request.body.email);
             const {meta} = jogadorExiste.data;
-            if(meta.pagination.total > 0) {
+            if (meta.pagination.total > 0) {
                 request.session.sessionFlash = {
                     type: "error",
                     style: "danger",
@@ -23,10 +28,29 @@ const cadastro = async (request, response) => {
                 return response.redirect("/cadastro")
             }
             const criarJogador = await cadastrarJogador(dados);
-            request.session.user = {
-                ...request.body
+            const url = `${process.env.FRONT_URL}/validar-email-jogador/${token}`;
+            const mensagem = `<strong>Olá ${request.body.nome},<br><br></strong>
+                  Para poder acessar o sistema de RPG, você precisa validar seu e-mail.<br>
+                  Para isso, basta <a href="${url}">clicar aqui</a>.<br><br><br>
+                  Caso não consiga clicar no link acima, copie e cole o endereço abaixo no navegador:<br><br>
+                  <em>${url}</em><br><br><br>
+                  Bons jogos!<br><br>
+                  Equipe Sistema RPG`;
+            const mail = await mailer.sendMail({
+                from: 'Sistema RPG <sistema@teste.com.br>', // sender address
+                to: `${request.body.nome} <${request.body.email}>`, // list of receivers
+                subject: `${request.body.nome}, estamos quase lá!`, // Subject line
+                html: mensagem, // html body
+
+            });
+
+            request.session.sessionFlash = {
+                type: "success",
+                style: "success",
+                message: "Cadastro realizado com sucesso! Verifique seu e-mail para validar!"
             }
-            return response.redirect("/");
+            return response.redirect("/cadastro");
+
         } catch (e) {
             if (typeof e.inner !== 'undefined') {
                 request.session.sessionFlash = {
@@ -56,7 +80,7 @@ const login = async (request, response) => {
             await validarLoginJogador().validate(request.body, {abortEarly: false});
             const logado = await loginAll(dados);
             const {user, status} = logado.data
-            console.log(user, status);
+
             if (status === "success") {
                 request.session.user = user;
                 if (user.perfil === "jogador") {
@@ -65,7 +89,7 @@ const login = async (request, response) => {
                     return response.redirect("/campanha");
                 }
             } else {
-                const { data } = logado.data
+                const {data} = logado.data
                 request.session.sessionFlash = {
                     type: "error",
                     style: "danger",
@@ -94,6 +118,35 @@ const logout = (request, response) => {
     return response.redirect("/login")
 }
 
+const validarEmailJogador = async (request, response) => {
+
+    try {
+        const token = request.params.token
+        const resultado = await pegarJogadorPorToken(token);
+        const jogador = {
+            data: {
+                ...resultado.data.data[0]
+            }
+        }
+
+        jogador.data.email_verificado = true;
+        const jogadorAlterado = await alterarJogador(jogador, jogador.data.id);
+        request.session.user = jogadorAlterado.data.data
+
+        const msg = `Parabéns ${jogadorAlterado.data.data.nome}. Já está tudo pronto para você usar o sistema. Você já pode
+            criar seu primeiro personagem <a href="/cadastro-personagem"> clicando aqui. </a> <br><br> Aproveite!!!`;
+        request.session.sessionFlash = {
+            type: "success",
+            style: "success",
+            message: msg
+        }
+
+        return response.redirect("/")
+    } catch (e) {
+        console.log(e);
+    }
+}
+
 export {
-    cadastro, login, logout
+    cadastro, login, logout, validarEmailJogador
 }
--- front/src/App/Services/jogadorService.js
+++ front/src/App/Services/jogadorService.js
@@ -3,9 +3,13 @@ import axios from "../../config/axios.js";
 const cadastrarJogador = (dados) => axios.post("/jogadores", dados);
 const loginAll = (dados) => axios.post("/jogadores/login", dados);
 const pegarJogadorPorEmail = (email) => axios.get(`/jogadores?filters[email]=${email}`);
+const pegarJogadorPorToken = (token) => axios.get(`/jogadores?filters[token]=${token}`);
+const alterarJogador = (jogador, id) => axios.get(`/jogadores/${id}`, jogador);
 
 export {
     cadastrarJogador,
     loginAll,
-    pegarJogadorPorEmail
+    pegarJogadorPorEmail,
+    pegarJogadorPorToken,
+    alterarJogador
 }
